d6681b5c3953e110c15f2ed906829a38d61e8854
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2024-12-01 21:36:10 -0500

    test

--- k_mer.py
+++ k_mer.py
@@ -18,28 +18,19 @@ def updateFunction(new_values, running_count):
         running_count = 0
     return sum(new_values) + running_count
 
-
 if __name__ == "__main__":
     # "local[2] = 2 threads to be used for local execution"
     # "StreamingContext(sc, 10) = 10 is the batch interval in seconds"
     # DS stream is created from the socket on localhost:9999
     # k mer of length 3 is generated from each line
-
     conf = SparkConf().setMaster("local[2]").setAppName("K-mer Count App")
     sc = SparkContext(conf=conf)
     ssc = StreamingContext(sc, 10)
-
     ssc.checkpoint("checkpoint_directory")
-
     lines = ssc.socketTextStream("localhost", 9999)
-
     kmers = lines.flatMap(lambda line: generate_kmers(line, 3))
-
     kmer_pairs = kmers.map(lambda kmer: (kmer, 1))
-    
     kmer_counts = kmer_pairs.updateStateByKey(updateFunction)
-    
     kmer_counts.pprint(num=100)
-
     ssc.start()
     ssc.awaitTermination()
