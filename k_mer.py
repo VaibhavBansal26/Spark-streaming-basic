6b8bae8d2373e1f3b8483ba7984eefea6b63d0cb
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2024-12-01 20:08:48 -0500

    k_mer cummulative

--- k_mer.py
+++ k_mer.py
@@ -1,31 +1,59 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
 
+# def generate_kmers(line, k=3):
+#     kmers = []
+#     words = line.strip().split()
+#     for word in words:
+#         for i in range(len(word) - k + 1):
+#             kmers.append(word[i:i+k])
+#     return kmers
+
 def generate_kmers(line, k=3):
     kmers = []
     words = line.strip().split()
     for word in words:
-        for i in range(len(word) - k + 1):
-            kmers.append(word[i:i+k])
+        word = word.lower()
+        if len(word) >= k:
+            for i in range(len(word) - k + 1):
+                kmers.append(word[i:i+k])
+        else:
+            kmers.append(word)  # Include shorter words as they are
     return kmers
+
+def updateFunction(new_values, running_count):
+    if running_count is None:
+        running_count = 0
+    return sum(new_values) + running_count
+
 
 if __name__ == "__main__":
     # "local[2] = 2 threads to be used for local execution"
     # "StreamingContext(sc, 10) = 10 is the batch interval in seconds"
     # DS stream is created from the socket on localhost:9999
     # k mer of length 3 is generated from each line
-    
+
     conf = SparkConf().setMaster("local[2]").setAppName("K-mer Count App")
     sc = SparkContext(conf=conf)
     ssc = StreamingContext(sc, 10)
+
+    ssc.checkpoint("checkpoint_directory")
 
     lines = ssc.socketTextStream("localhost", 9999)
 
     kmers = lines.flatMap(lambda line: generate_kmers(line, 3))
 
-    kmer_counts = kmers.map(lambda kmer: (kmer, 1)).reduceByKey(lambda a, b: a + b)
+    # kmer_counts = kmers.map(lambda kmer: (kmer, 1)).reduceByKey(lambda a, b: a + b)
 
-    kmer_counts.pprint()
+    # kmer_counts.pprint()
+
+    kmer_pairs = kmers.map(lambda kmer: (kmer, 1))
+    
+    # Update the cumulative counts
+    kmer_counts = kmer_pairs.updateStateByKey(updateFunction)
+    
+    # Print the cumulative k-mer counts
+    kmer_counts.pprint(num=100)
 
     ssc.start()
     ssc.awaitTermination()
